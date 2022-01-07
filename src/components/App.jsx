a89ae30a353ff929920558e616ffbc6668bf0cb1
Rishi Patel <patelrishi989@gmail.com>
2022-01-07 12:09:49 +0530

    cleaned the code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,21 +2,23 @@ import "../styles/App.css";
 import "../index.css";
 
 function App() {
-	return (
-		<div className="container">
-			<header className="App-header">
-				<h1>Twitter Utility</h1>
-			</header>
-			<div class="log-form">
-				<h2>Login to your account</h2>
-				<form>
-					<input type="text" title="username" placeholder="username" />
-					<input type="password" title="username" placeholder="password" />
-					<button type="submit" class="btn">Login</button>
-				</form>
-			</div>
-		</div>
-	);
+  return (
+    <div className="container">
+      <header className="App-header">
+        <h1>Twitter Utility</h1>
+      </header>
+      <div class="log-form">
+        <h2>Login to your account</h2>
+        <form>
+          <input type="text" title="username" placeholder="username" />
+          <input type="password" title="username" placeholder="password" />
+          <button type="submit" class="btn">
+            Login
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default App;
