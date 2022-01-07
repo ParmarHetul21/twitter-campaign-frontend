193999a1ad394893bd09c9e97a1c46ef1e39ae52
ParmarHetul21 <hetul.d.parmar.20@gmail.com>
2022-01-07 11:27:08 +0530

    header added

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import "../styles/App.css";
+import "../index.css";
+
+function App() {
+	return (
+		<div className="App-header">
+			<header>
+				<h1>Twitter Utility</h1>
+			</header>
+		</div>
+	);
+}
+
+export default App;
