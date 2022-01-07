a4bc9449b271204f34d29d0809efb7a83610589e
Rishi Patel <patelrishi989@gmail.com>
2022-01-07 12:06:28 +0530

    Added login page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,10 +3,18 @@ import "../index.css";
 
 function App() {
 	return (
-		<div className="App-header">
-			<header>
+		<div className="container">
+			<header className="App-header">
 				<h1>Twitter Utility</h1>
 			</header>
+			<div class="log-form">
+				<h2>Login to your account</h2>
+				<form>
+					<input type="text" title="username" placeholder="username" />
+					<input type="password" title="username" placeholder="password" />
+					<button type="submit" class="btn">Login</button>
+				</form>
+			</div>
 		</div>
 	);
 }
