2f76e506a794b6d3e658f345efecac93e4f5b57e
ParmarHetul21 <hetul.d.parmar.20@gmail.com>
2022-01-10 14:52:29 +0530

    implemented react table and tested on 16000 rows

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,28 +1,63 @@
 import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
+	const [name, setname] = useState("");
+	const [password, setpassword] = useState("");
+	const navigate = useNavigate();
+	var error = document.getElementById("error");
+
+	const checkUsername = e => {
+		setname(e.target.value);
+	};
+
+	const checkPassword = e => {
+		setpassword(e.target.value);
+	};
+
+	const onLoginChnage = e => {
+		if (name === "pratik" && password === "Jain@123") {
+			navigate("/table");
+		} else if (name === "" && password === "") {
+			error.style.display = "block";
+		} else {
+			error.style.display = "block";
+		}
+	};
+
 	return (
 		<div className="container">
 			<header className="App-header">
 				<h1>Twitter Utility</h1>
 			</header>
-			<div class="log-form">
+			<div className="log-form">
 				<h2>Login to your account</h2>
 				<form>
 					<input
 						type="text"
 						title="username"
 						placeholder="username"
+						onChange={checkUsername}
+						required
 					/>
 					<input
 						type="password"
 						title="username"
 						placeholder="password"
+						onChange={checkPassword}
+						required
 					/>
-					<button type="submit" class="btn">
+					<button
+						type="submit"
+						className="btn"
+						onClick={onLoginChnage}
+					>
 						Login
 					</button>
 				</form>
+				<p className="error">username and password are wrong</p>
+				<p className="error">username and password are empty</p>
 			</div>
 		</div>
 	);
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,9 +1,91 @@
-import React from "react";
+import { React, useMemo } from "react";
+import { useTable } from "react-table";
+import campaignData from "../source/campaign_data.json";
 
 export default function Table() {
+	const columns = useMemo(
+		() => [
+			{
+				Header: "users",
+				columns: [
+					{
+						Header: "id",
+						accessor: "id"
+					},
+					{
+						Header: "followers_count",
+						accessor: "public_metrics.followers_count"
+					},
+					{
+						Header: "following_count",
+						accessor: "public_metrics.following_count"
+					},
+					{
+						Header: "tweet_count",
+						accessor: "public_metrics.tweet_count"
+					},
+					{
+						Header: "listed_count",
+						accessor: "public_metrics.listed_count"
+					},
+					{
+						Header: "name",
+						accessor: "name"
+					},
+					{
+						Header: "username",
+						accessor: "username"
+					}
+				]
+			}
+		],
+		[]
+	);
+
+	const data = useMemo(() => campaignData.users, []);
+
+	const {
+		getTableProps,
+		getTableBodyProps,
+		headerGroups,
+		rows,
+		prepareRow
+	} = useTable({
+		columns,
+		data
+	});
+
 	return (
-		<div>
-			<h1>Table</h1>
+		<div id="tweets">
+			<table {...getTableProps()} id="table">
+				<thead>
+					{headerGroups.map(headerGroup =>
+						<tr {...headerGroup.getHeaderGroupProps()}>
+							{headerGroup.headers.map(column =>
+								<th {...column.getHeaderProps()}>
+									{column.render("Header")}
+								</th>
+							)}
+						</tr>
+					)}
+				</thead>
+				<tbody {...getTableBodyProps()}>
+					{rows.map((row, i) => {
+						prepareRow(row);
+						return (
+							<tr {...row.getRowProps()}>
+								{row.cells.map(cell => {
+									return (
+										<td {...cell.getCellProps()}>
+											{cell.render("Cell")}
+										</td>
+									);
+								})}
+							</tr>
+						);
+					})}
+				</tbody>
+			</table>
 		</div>
 	);
 }
